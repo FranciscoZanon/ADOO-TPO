910e9f348e7bc0d45b3358aa9e5422f0cb486733
PerezzL <lucasperezmarquez1@gmail.com>
2024-06-24 17:14:10 -0300

    ParametroFacturacion

--- src/modelos/ParametroFacturacion.java
+++ src/modelos/ParametroFacturacion.java
@@ -1,5 +1,28 @@
 package modelos;
 
 public class ParametroFacturacion {
+    private float precioReserva;
+    private Habitacion habitacion;
 
+    public ParametroFacturacion(float precioReserva, Habitacion habitacion) {
+        this.precioReserva = precioReserva;
+        this.habitacion = habitacion;
+    }
+
+    // Getters y Setters
+    public float getPrecioReserva() {
+        return precioReserva;
+    }
+
+    public void setPrecioReserva(float precioReserva) {
+        this.precioReserva = precioReserva;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
 }
